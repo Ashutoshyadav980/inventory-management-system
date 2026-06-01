ccc8f178565a77888fbfba6e648922dbd0632afe
Ashutosh Yadav <ashutoshy980711@gmail.com>
2026-06-02 04:18:45 +0530

    fix cors configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://inventory-management-system-lilac-pi.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
